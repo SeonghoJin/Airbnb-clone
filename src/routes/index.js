1dd8d687e82aa6151273201ba3494631bbfeb6a7
SeonghoJin <seongho4996@naver.com>
2021-05-15 11:15:16 +0900

    route auth prototype 작성

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import path from 'path'
+import auth from './auth.js'
 
 const router = Router();
+auth(router);
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(path.resolve(), "/src/static/index.html"));
