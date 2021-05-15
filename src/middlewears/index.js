19c4d1b9ba884b1f4d020defd9f7cb0ae73df335
SeonghoJin <seongho4996@naver.com>
2021-05-15 22:31:04 +0900

    add json parser at express app

--- src/middlewears/index.js
+++ src/middlewears/index.js
@@ -1,7 +1,9 @@
 import allowStatic from './allowStatic.js'
 import session from './session.js'
+import express from 'express'
 
 export default (expressApp) => {
     allowStatic(expressApp);
     session(expressApp)
+    expressApp.use(express.json())
 }
