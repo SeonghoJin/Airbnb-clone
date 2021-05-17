d5c5bc942d7deaee69e249a193458b5c5807f99f
SeonghoJin <seongho4996@naver.com>
2021-05-18 00:20:27 +0900

    config 추가

--- src/config/index.js
+++ src/config/index.js
@@ -2,13 +2,16 @@ import dotenv from 'dotenv';
 
 const envs = dotenv.config();
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 if (envs.error) {
     throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 const config = {
     port: process.env.PORT,
-    session_secret: process.env.SESSION_SECRET
+    session_secret: process.env.SESSION_SECRET,
+    MAIN_URI: (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '')
 }
 
 export default config
