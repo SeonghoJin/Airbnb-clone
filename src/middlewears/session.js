628be93222afc8292d5f8497e47fb3fc850c8784
SeonghoJin <seongho4996@naver.com>
2021-05-18 11:54:56 +0900

    session 옵션 변경

--- src/middlewears/session.js
+++ src/middlewears/session.js
@@ -6,7 +6,7 @@ export default (app) => {
     const nedbStore = new NedbStore(session);
     app.use(session({
         resave: true,
-        saveUninitialized: true,
+        saveUninitialized: false,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 31
         },
