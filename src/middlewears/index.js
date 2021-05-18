b0a993fba4274eccb13a8e34f7c4dabfc9d11375
SeonghoJin <seongho4996@naver.com>
2021-05-18 11:52:55 +0900

    cookie-parser 모듈 추가

--- src/middlewears/index.js
+++ src/middlewears/index.js
@@ -2,9 +2,12 @@ import allowStatic from './allowStatic.js'
 import session from './session.js'
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 export default (expressApp) => {
     allowStatic(expressApp);
     session(expressApp)
     expressApp.use(express.json())
     expressApp.use(cors());
+    expressApp.use(cookieParser());
 }
